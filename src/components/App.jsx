ab61a8063a62b355c0d0bcddfa1cbc4d2d538f05
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-09-26 20:07:07 +0300

    рефакторит код на хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect} from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm  from './contactForm';
 import ContactList  from './contactList';
 import Filter from './filter';
 
-export class App extends Component {
+export class App1 extends Component {
   state = {
     contacts: [
     ],
@@ -45,10 +45,6 @@ export class App extends Component {
     this.setState(p => ({
       contacts: [contact, ...p.contacts],
     }));
-    // const contactsLS = localStorage.getItem('contacts');
-    // const parsedContacts = JSON.parse(contactsLS);
-    // const allContacts = [...parsedContacts, contact]
-    // localStorage.setItem('contacts', JSON.stringify(allContacts));
   };
 
   deleteContact = id => {
@@ -82,4 +78,64 @@ export class App extends Component {
       </>
     );
   }
+}
+
+export default function App() {
+  const [filter, setFilter] = useState('');
+
+  const [contacts, setContacts] = useState(localStorage.getItem('contacts') 
+  ? JSON.parse(localStorage.getItem('contacts')) 
+  : []
+  );
+
+  // useEffect(()=>{
+    // const contacts = localStorage.getItem('contacts');
+    // const parsedContacts = JSON.parse(contacts);
+    // if (parsedContacts) {
+    //   setContacts([parsedContacts]);
+    // }
+  // }, []);
+
+  useEffect(()=> {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    if((contacts.find(contact => (contact.name.includes(name))))) return alert(`${name} is already in contacts.`);
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    
+    setContacts(prevState => ([contact, ...prevState]));
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => (prevState.filter(contact => contact.id !== id)))
+    this.setState(p => ({
+      contacts: p.contacts.filter(contact => contact.id !== id)
+    }))
+
+    setContacts(prevState => (prevState.filter(contact => contact.id !== id)))
+  }
+
+  const changeFilter = event => {
+    const {value} = event.currentTarget;
+    setFilter(value);
+  }
+
+const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+
+return (
+  <>
+    <h1>Phonebook</h1>
+      <ContactForm  onSubmit={addContact}/>
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter}/>
+      <ContactList  contacts={visibleContacts} onDeleteContact={deleteContact} />
+  </>
+);
 }
--- src/components/contactList/ContactList .js
+++ src/components/contactList/ContactList .js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {DeleteButton} from './ContactList .module';
 
-class ContactList extends Component {
-  render() {
-    return (
-      <>
-        <ul>
-          {this.props.contacts.map(contact => {
-            return (
-              <li key={contact.id}>
-                {contact.name}: {contact.number}
-                <DeleteButton type="button" onClick={() => this.props.onDeleteContact(contact.id)}>delete</DeleteButton>
-              </li>
-            );
-          })}
-        </ul>
-      </>
-    );
-  }
+// class ContactList extends Component {
+//   render() {
+//     return (
+//       <>
+//         <ul>
+//           {this.props.contacts.map(contact => {
+//             return (
+//               <li key={contact.id}>
+//                 {contact.name}: {contact.number}
+//                 <DeleteButton type="button" onClick={() => this.props.onDeleteContact(contact.id)}>delete</DeleteButton>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </>
+//     );
+//   }
+// }
+
+function ContactList(options) {
+  const {contacts, onDeleteContact} = options;
+  console.log('contacts', contacts);
+  console.log('options', options);
+
+  return (
+    <>
+      <ul>
+        {contacts && contacts.map(contact => {
+          return (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+              <DeleteButton type="button" onClick={() => onDeleteContact(contact.id)}>delete</DeleteButton>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default ContactList;
